b09f21cac04ef11a9fc4d5743ffcece392cf758c
Vitaliy L <lops@list.ru>
2026-03-11 01:10:38 +0300

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    inlineStylesheets: "always",
+  },
+  vite: {
+    build: {
+      cssCodeSplit: true,
+    },
+    css: {
+      devSourcemap: true,
+    },
+  },
+  devToolbar: {
+    enabled: false,
+  },
+});
